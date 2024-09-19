0f91de5a0eab61b493674d85b4fbc26e80a6d235
Sachiko Suzuki <>
2024-09-19 10:13:49 +0900

    ロギングを追加

---
+++ src/utils/logger.ts
@@ -0,0 +1,39 @@
+import pino from 'pino';
+import { createPinoBrowserSend, createWriteStream } from 'pino-logflare';
+
+if (
+  !process.env.NEXT_PUBLIC_LOGFLARE_API_KEY ||
+  !process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID
+) {
+  throw new Error(
+    'Logflare API key or Source ID is not set in environment variables.',
+  );
+}
+
+const stream = createWriteStream({
+  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
+  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID,
+});
+
+const send = createPinoBrowserSend({
+  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
+  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID,
+});
+
+const logger = pino(
+  {
+    browser: {
+      transmit: {
+        level: 'info',
+        send: send,
+      },
+    },
+    level: 'debug',
+    base: {
+      env: process.env.NODE_ENV,
+    },
+  },
+  stream,
+);
+
+export default logger;
